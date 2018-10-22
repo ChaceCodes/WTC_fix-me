ce84234ce0bf541930424be90715733308c234f7
Chace GREEN <chgreen@c1r2s6.wethinkcode.co.za>
2018-10-22 10:22:42 +0200

    basic router complete from tutorial, listens for new connections from broker

--- fixme/fix-me-broker/src/main/java/com/chgreen/app/Broker.java
+++ fixme/fix-me-broker/src/main/java/com/chgreen/app/Broker.java
@@ -2,17 +2,37 @@ package com.chgreen.app;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.Socket;
-import java.util.Scanner;
-import com.chgreen.app.PromptUser;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.charset.Charset;
+import java.util.concurrent.Future;
+//import com.chgreen.app.PromptUser;
 
 public class Broker 
 {
     
     public static void main( String[] args ) throws Exception
     {
-        PromptUser userPrompt = new PromptUser();
+        AsynchronousSocketChannel channel = AsynchronousSocketChannel.open();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        /*PromptUser userPrompt = new PromptUser();
         Scanner scanner = new Scanner(System.in);
         Socket sockRouter = new Socket("localhost", 5000);
         PrintStream PSRouter = new PrintStream(sockRouter.getOutputStream());
@@ -35,6 +55,7 @@ public class Broker
 
             String message = BRRouter.readLine();
             System.out.println(message);
-        }
+        }*/
     }
 }
+
--- fixme/fix-me-router/src/main/java/com/chgreen/app/Router.java
+++ fixme/fix-me-router/src/main/java/com/chgreen/app/Router.java
@@ -2,12 +2,36 @@ package com.chgreen.app;
 
 import java.io.*;
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.AsynchronousServerSocketChannel;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.nio.charset.Charset;
+
 
 public class Router 
 {
     public static void main( String[] args ) throws Exception
     {
-        int totalBrokers = 0;
+        AsynchronousServerSocketChannel server = AsynchronousServerSocketChannel
+        .open();
+        String host = "localhost";
+        int portBroker = 5000;
+        int portMarket = 5001;
+        InetSocketAddress sAddr = new InetSocketAddress(host, portBroker);       
+        server.bind(sAddr);      
+        System.out.format("Server is listening at %s%n", sAddr);
+        Attachment attach = new Attachment();
+        attach.server = server;
+        server.accept(attach, new ConnectionHandler());
+        Thread.currentThread().join();
+        
+        
+        
+        
+        
+        
+        /*int totalBrokers = 0;
         int totalMarkets = 0;
         ServerSocket serverSocketBroker = new ServerSocket(5000);
         Socket socketBroker = serverSocketBroker.accept();
@@ -28,7 +52,7 @@ public class Router
                 PrintStream PSBroker = new PrintStream(socketBroker.getOutputStream());
                 PSBroker.println("Message received!");
             }
-        }
+        }*/
     }
 
     private int validateCheckSum(){
@@ -40,4 +64,85 @@ public class Router
 
         return(0);
     } 
+}
+
+class Attachment {
+    AsynchronousServerSocketChannel server;
+    AsynchronousSocketChannel client;
+    ByteBuffer buffer;
+    SocketAddress clientAddr;
+    boolean isRead;
+}
+
+class ConnectionHandler implements CompletionHandler<AsynchronousSocketChannel, Attachment>{
+
+    @Override
+    public void completed(AsynchronousSocketChannel client, Attachment attach) {
+        try{
+            SocketAddress clientAddr = client.getRemoteAddress();
+            System.out.format("Accepted a  connection from  %s%n", clientAddr);
+            attach.server.accept(attach, this);
+            ReadWriteHandler rwHandler = new ReadWriteHandler();
+            Attachment newAttach = new Attachment();
+            newAttach.server = attach.server;
+            newAttach.client = client;
+            newAttach.buffer = ByteBuffer.allocate(2048);
+            newAttach.isRead = true;
+            newAttach.clientAddr = clientAddr;
+            client.read(newAttach.buffer, newAttach, rwHandler);            
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void failed(Throwable exc, Attachment attachment) {
+            System.out.println("Failed to accept a connection.");
+            exc.printStackTrace();
+    }
+
+}
+
+class ReadWriteHandler implements CompletionHandler<Integer, Attachment>{
+
+    @Override
+    public void completed(Integer result, Attachment attach) {
+        if (result == -1){
+            try{
+                attach.client.close();
+                System.out.format("Stopped   listening to the   client %s%n",attach.clientAddr);
+            }
+            catch(IOException e){
+                e.printStackTrace();
+            }
+            return;
+        }
+
+        if (attach.isRead){
+            attach.buffer.flip();
+            int limits = attach.buffer.limit();
+            byte[] bytes = new byte[limits];
+            attach.buffer.get(bytes, 0, limits);
+            Charset cs = Charset.forName("UTF-8");
+            String msg = new String(bytes, cs);
+            System.out.format("Client at  %s  says: %s%n", attach.clientAddr, msg);
+            attach.isRead = false;
+            attach.buffer.rewind();
+        }
+        else {
+            attach.client.write(attach.buffer, attach, this);
+            attach.isRead = true;
+            attach.buffer.clear();
+            attach.client.read(attach.buffer, attach, this);
+        }
+
+    }
+
+    @Override
+    public void failed(Throwable exc, Attachment attach) {
+            exc.printStackTrace();
+    }
+
 }
